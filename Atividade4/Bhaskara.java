b9e3bd9e1a8828d5306b177303f867721ee04a2b
Juliano Tavares <156096676+JulianoTavaresDaSilva@users.noreply.github.com>
2025-06-16 21:40:09 +0000

    fim da atividade 4

--- Atividade4/Bhaskara.java
+++ Atividade4/Bhaskara.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 public class Bhaskara {
-    public static void main (String[] args){
-        Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Valor de a: ");
-        double a = entrada.nextDouble();
+        System.out.print("Digite o valor de a: ");
+        double a = scanner.nextDouble();
 
-        System.out.print("Valor de b: ");
-        double b = entrada.nextDouble();
+        System.out.print("Digite o valor de b: ");
+        double b = scanner.nextDouble();
 
-        System.out.print("Valor de c: ");
-        double c = entrada.nextDouble();
+        System.out.print("Digite o valor de c: ");
+        double c = scanner.nextDouble();
 
-        double delta = b**2 - 4 * a * c;
-        
+        double delta = (b * b) - (4 * a * c);
+        System.out.println("Delta: " + delta);
+
+        if (delta < 0) {
+            System.out.println("Não existem raízes reais.");
+        } else {
+            double raizDelta = Math.sqrt(delta);
+            double x1 = (-b + raizDelta) / (2 * a);
+            double x2 = (-b - raizDelta) / (2 * a);
+
+            System.out.println("As raízes são:");
+            System.out.println("x' = " + x1);
+            System.out.println("x'' = " + x2);
+        }
+
+        scanner.close();
     }
 }
